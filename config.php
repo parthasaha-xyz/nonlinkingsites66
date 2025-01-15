331852d578144ad9d91f12b1e13137f911429e2b
parthasaha-xyz <97681941+parthasaha-xyz@users.noreply.github.com>
2025-01-15 14:23:34 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,1 @@
-<?php
 
-/* Configuration of the site */
-define('DATABASE_SERVER',   '');
-define('DATABASE_USERNAME', '');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     '');
-define('SITE_URL',          '');
